23ec9d02fe4cb0809cd01533810541c96aa0552f
k_eguchi <k_eguchi@zoho.com>
2015-07-21 14:09:09 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
-Rails.application.routes.draw do
-  root  'static_pages#home'
-  
+SampleApp::Application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
-  devise_for :users
+  root 'static_pages#home'
+
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
